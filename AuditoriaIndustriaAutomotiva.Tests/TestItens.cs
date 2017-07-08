3b9dde83ca297a62d289d2310548eaf9ccc793fa
Rodrigo Silva <rodrigmars@gmail.com>
2017-07-07 22:52:06 -0300

    Criado projeto de teste

---
+++ AuditoriaIndustriaAutomotiva.Tests/TestItens.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AuditoriaIndustriaAutomotiva.Tests
+{
+    public class TestItens
+    {
+
+        [Fact]
+        public void VerificaItemDeVenda() {
+
+            var ItemVendaId = 15;
+
+            Assert.Equal(15, ItemVendaId);
+        }
+    }
+}
